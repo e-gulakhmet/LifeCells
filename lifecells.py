474c2c3983fb851b4b1eb8d052442a76206ca9ab
EnesGUL <enes515254@gmail.com>
2018-01-05 22:02:22 +0600

    Draw colony on display

--- lifecells.py
+++ lifecells.py
@@ -15,7 +15,7 @@ import colony
 def grp_init():
     pygame.init()
 
-    size=[700,500]
+    size=[700, 500]
     screen=pygame.display.set_mode(size)
     pygame.display.set_caption("Life Cells demonstration")
 
@@ -39,6 +39,35 @@ def init_space():
 
     return space
 
+def draw_col(screen, x, y, col):
+    """
+    Draws single colony
+
+    Draws single colony at given position
+    """
+    c_Cell = [
+              (0, 75, 0),
+              (0, 255, 0),
+              (0, 235, 0),
+              (0, 215, 0),
+              (0, 195, 0),
+              (0, 175, 0),
+              (0, 155, 0),
+              (0, 135, 0),
+              (0, 115, 0),
+              (0, 95, 0),
+             ]
+    x = int((70 - col[0][3])/2)
+    y = int((50 - col[0][4])/2)
+    for yy, row in enumerate(col[1]):
+        for xx, cell in enumerate(row):
+            if cell[0] != 0:
+                if cell[0] > 9:
+                    cIdx = 0
+                else:
+                    cIdx = cell[0]
+                pygame.draw.circle(screen, c_Cell[cIdx], ((x + xx)*10 + 5, (y + yy)*10 + 5), 5)
+
 
 
 def run():
@@ -53,6 +82,7 @@ def run():
     c_headerTxt      = (   0,   0,   0)
     c_valueTxt       = ( 255, 255, 255)
     c_splitLine      = (   0, 255,   0)
+    c_bkGround       = (   0,   0,   0)
     c_cells         = [(),
                        ()
                       ] 
@@ -66,10 +96,14 @@ def run():
     # списком [0,0,0]
     # Заметьте: эта строка создаёт картинку с буквами,
     # но пока не выводит её на экран.
-    text = font.render("My text", True, c_valueTxt)
+    # text = font.render("My text", True, c_valueTxt)
     
     # Вывести сделанную картинку на экран в точке (250, 250)
-    screen.blit(text, [250,250])
+    # screen.blit(text, [250,250])
+
+    active_col = 0
+
+    v_shift, h_shift = 0, 0
 
     done = False
     # -------- Main Program Loop -----------
@@ -79,7 +113,26 @@ def run():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
-    
+                continue
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_p:
+                    active_col -= 1
+                    if active_col < 0:
+                        active_col = 0
+                elif event.key == pygame.K_n:
+                    active_col += 1
+                    if active_col > len(space) - 3:
+                        active_col = len(space) - 3
+                elif event.key == pygame.K_q:
+                    done = True
+                    continue
+                elif event.key == pygame.K_UP:
+                    if pygame.key.get_mods() & pygame.KMOD_CTRL:
+                        v_shift = -10
+                    else:
+                        v_shift = -1
+                
         # --- Game logic should go here
     
         # --- Screen-clearing code goes here
@@ -89,15 +142,21 @@ def run():
     
         # If you want a background image, replace this clear with blit'ing the
         # background image.
-        # screen.fill(WHITE)
+        screen.fill(c_bkGround)
     
         # --- Drawing code should go here
+        draw_col(screen, 0, 0, space[2 + active_col])
     
         # --- Go ahead and update the screen with what we've drawn.
         pygame.display.flip()
+
+        colony.next_day(space)
+
+        if active_col > len(space) - 3:
+            active_col = len(space) -3
     
         # --- Limit to 60 frames per second
-        clock.tick(60)
+        clock.tick(1)
     
     # Close the window and quit.
     pygame.quit()   
@@ -114,7 +173,7 @@ def main():
     
     """
     logging.basicConfig(filename="lifecells.log",
-                        level=logging.DEBUG,
+                        level=logging.ERROR,
                         format="%(asctime)s [%(levelname)s] : %(message)s")
      
     run()
