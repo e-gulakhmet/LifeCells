63c932297d1854cccf65c6b63c42101e8d8afa54
dr-dobermann <rgabitov@gmail.com>
2018-01-16 00:31:04 +0600

    Fix bug with centering of viewport

--- lifecells.py
+++ lifecells.py
@@ -39,7 +39,15 @@ MINIMAP_SIZE = 150
 KEY_DELAY = 100
 KEY_INTERVAL = 100
 
+# Минимальные размеры экрана
+SCR_MIN_WIDTH = 700
+SCR_MIN_HEIGHT = 500
 
+# Отступы экрана
+N_OFFSET = 40
+E_OFFSET = 30
+S_OFFSET = 30
+W_OFFSET = 0
 
 def grp_init(size):
     """
@@ -88,7 +96,10 @@ def init_space(name = "Universe"):
 
 
 def vport_center_on(vport, active_col):
-    if (len(vport[0]) > 2 and active_col >= 0 and active_col <= len(vport[0]) - 3):
+    """
+    Centers viewport over the active colony
+    """
+    if active_col >= 0 and active_col <= len(vport[0]) - 3:
         # Найти центр активной колонии
         #       xc = col_x + col_w / 2
         #       yc = col_y + col_h / 2
@@ -105,18 +116,22 @@ def vport_center_on(vport, active_col):
             x = 0
         if y < 0:
             y = 0
-        vport[2], vport[3] = x, y 
+        vport[2], vport[3] = x, y
+
         ws, hs = get_space_size(vport[0])
         if vport[2] + vport[4] > ws:
             vport[2] = ws - vport[4]
         if vport[3] + vport[5] > hs:
-            vport[3] = ws - vport[5]
+            vport[3] = hs - vport[5]
     else:
         vport[2], vport[3] = 0, 0
 
 
 
 def update_vport_size(vport):
+    """
+    Updates viewport size according to screen size
+    """
     # Получить размеры экрана
     s_rect = vport[1].get_rect()
     # Отнять отступы (offsets)
@@ -125,11 +140,6 @@ def update_vport_size(vport):
     # Разделить на размер одной клетки
     w = int(w / CELL_SIZE)
     h = int(h / CELL_SIZE)
-    ws, hs = get_space_size(vport[0])
-    if ws < w:
-        w = ws
-    if hs < h:
-        h = hs
     # Найти левый-верхний край viewport
     #       xv = xv - (w - vport_w) / 2
     #       yv = yv - (h - vport_h) / 2
@@ -138,10 +148,12 @@ def update_vport_size(vport):
     vport[4] = w
     vport[5] = h
     # Проверить выход viewport за границы экрана
-    if vport[3] < 0:
-        vport[3] = 0
     if vport[2] < 0:
         vport[2] = 0
+    if vport[3] < 0:
+        vport[3] = 0
+
+    ws, hs = get_space_size(vport[0])
     if vport[2] + vport[4] > ws:
         vport[2] = ws - vport[4]
     if vport[3] + vport[5] > hs:
@@ -149,7 +161,6 @@ def update_vport_size(vport):
 
 
 
-# TODO: Add scrollable viewport for the space
 def viewport_init(space, active_col, screen, offset):
     """
     Creates viewport for space.
@@ -179,7 +190,14 @@ def viewport_init(space, active_col, screen, offset):
 # TODO: Add scrollbars on the right and bottom side of the screen
 # TODO: Add statistics on screen
 
-def draw_minimap(vport, size):
+def draw_minimap(vport):
+    """
+    Draws a minimap
+    Draws minimap in left-bottom corner
+    """
+    s_rect = vport[1].get_rect()
+    size = (s_rect.w, s_rect.h)
+
     surf = vport[1].subsurface(
             pygame.Rect(0, size[1] - MINIMAP_SIZE - 3,
                         MINIMAP_SIZE + 3, MINIMAP_SIZE + 3))
@@ -195,10 +213,13 @@ def draw_minimap(vport, size):
     # Если высота space больше чем его ширина, то отступы будут слева и справа
     if h > w:
         v_offset = int((MINIMAP_SIZE - w / (h / MINIMAP_SIZE)) / 2)
+
+    # Нарисовать пространство
     pygame.draw.rect(surf, C_MM_SPACE,
-                     pygame.Rect(1 + v_offset, 1 + h_offset, 
-                                 MINIMAP_SIZE - 2 * v_offset, 
-                                 MINIMAP_SIZE - 2 * h_offset))     
+                     pygame.Rect(1 + v_offset, 1 + h_offset,
+                                 MINIMAP_SIZE - 2 * v_offset,
+                                 MINIMAP_SIZE - 2 * h_offset))
+
     # Нарисовать рамку viewport
     if w > h:
         scale = w / MINIMAP_SIZE
@@ -208,8 +229,9 @@ def draw_minimap(vport, size):
                      pygame.Rect(1 + v_offset + int(vport[2] / scale),
                                  1 + h_offset + int(vport[3] / scale),
                                  int(vport[4] / scale),
-                                 int(vport[5] / scale)), 
-                     1)    
+                                 int(vport[5] / scale)),
+                     1)
+
     # Нарисовать рамку minimap
     pygame.draw.rect(surf, C_MM_BORDER,
                      pygame.Rect(0, 0, MINIMAP_SIZE + 2, MINIMAP_SIZE + 2), 1)
@@ -227,8 +249,7 @@ def draw_minimap(vport, size):
 
 
 
-
-def draw_vport(vport, size):
+def draw_vport(vport):
     """
     Draw viewport.
 
@@ -246,6 +267,9 @@ def draw_vport(vport, size):
               (0, 115, 0),
               (0, 95, 0),
              ]
+
+    s_rect = vport[1].get_rect()
+    size = (s_rect.w, s_rect.h)
 
     surf = vport[1].subsurface(
             pygame.Rect(vport[6][3], vport[6][0],
@@ -341,14 +365,12 @@ def run():
     # viewPort shift
     v_shift, h_shift = 0, 0
 
-    # size хранит текушие размеры окна приложения в точках [w, h]
-    size = [700, 500]
-
     space = init_space()
 
-    screen = grp_init(size)
+    screen = grp_init((SCR_MIN_WIDTH, SCR_MIN_HEIGHT))
 
-    vport = viewport_init(space, active_col, screen, (40, 30, 30, 0))
+    vport = viewport_init(space, active_col, screen, 
+                         (N_OFFSET, E_OFFSET, S_OFFSET, W_OFFSET))
 
     clock = pygame.time.Clock()
     # space day change speed
@@ -369,6 +391,8 @@ def run():
 
         # --- Main event loop
         for event in pygame.event.get():
+            # Обработать события таймера,
+            # запускающего новый день
             if event.type == pygame.USEREVENT:
                 newDay = True
 
@@ -379,10 +403,10 @@ def run():
             # Обработать событие изменения размеров экрана
             if event.type == pygame.VIDEORESIZE:
                 size = list(event.dict["size"])
-                if size[0] < 700:
-                    size[0] = 700
-                if size[1] < 500:
-                    size[1] = 500    
+                if size[0] < SCR_MIN_WIDTH:
+                    size[0] = SCR_MIN_WIDTH
+                if size[1] < SCR_MIN_HEIGHT:
+                    size[1] = SCR_MIN_HEIGHT    
                 screen = pygame.display.set_mode(size, pygame.DOUBLEBUF | pygame.RESIZABLE)
                 vport[1] = screen
                 update_vport_size(vport)
@@ -393,15 +417,20 @@ def run():
                     active_col -= 1
                     if active_col < 0:
                         active_col = 0
+
                 elif event.key == pygame.K_n: # select next colony as active
                     active_col += 1
                     if active_col > len(space) - 3:
                         active_col = len(space) - 3
-                elif event.key == pygame.K_SPACE:
+
+                # center viewport on the active_col
+                elif event.key == pygame.K_SPACE: 
                     vport_center_on(vport, active_col)
+
                 elif event.key == pygame.K_q: # quit 
                     done = True
                     continue
+
                 elif event.key == pygame.K_UP:   # shift viewport up
                     if pygame.key.get_mods() & pygame.KMOD_CTRL:
                         v_shift -= 10
@@ -422,6 +451,7 @@ def run():
                         v_shift += 10
                     else:
                         v_shift += 1
+
                 elif event.key == pygame.K_s:   # make speed slower
                     if curr_speed > 0:
                         curr_speed -= 1
@@ -454,28 +484,22 @@ def run():
             else:
                 if h_shift != 0:
                     vport[2] += h_shift
-                    if vport[2] + vport[4] - 1 > w:
-                        vport[2] = w - vport[4]
                     if vport[2] < 0:
                         vport[2] = 0
+                    if vport[2] + vport[4] - 1 > w:
+                        vport[2] = w - vport[4]
                 
             if vport[5] >= h:
                 vport[3] = 0
             else:   
                 if v_shift != 0:
                     vport[3] += v_shift
-                    if vport[3] + vport[5] - 1 > h:
-                        vport[3] = h - vport[5]
                     if vport[3] < 0:
                         vport[3] = 0
+                    if vport[3] + vport[5] - 1 > h:
+                        vport[3] = h - vport[5]
             v_shift = 0
             h_shift = 0
-        
-            
-
-
-
-
 
         # --- Screen-clearing code goes here
         screen.fill(C_BKGROUND)
@@ -487,8 +511,8 @@ def run():
         
 
         # --- Drawing code should go here      
-        draw_vport(vport, size)
-        draw_minimap(vport, size)
+        draw_vport(vport)
+        draw_minimap(vport)
 
 
         # --- Go ahead and update the screen with what we've drawn.
@@ -521,8 +545,6 @@ def run():
 def main():
     """
     Programm entry point
-
-    
     """
     logging.basicConfig(filename="lifecells.log",
                         level=logging.ERROR,
